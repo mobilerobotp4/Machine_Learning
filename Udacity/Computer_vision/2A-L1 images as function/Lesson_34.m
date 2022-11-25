0ab2a23267a0c65e02eaecd83aced6b6965d7be5
Saswati Bhattacharjee <36052453+mobilerobotp4@users.noreply.github.com>
2022-11-25 00:03:01 -0600

    Delete Udacity directory

--- Udacity/Computer_vision/2A-L1 images as function/Lesson_34.m
+++
@@ -1,7 +0,0 @@
-%Apply Gaussian noise to an image
-img = imread('Fruit.jpg');
-imshow(img);
-
-noise= randn(size(img)) .*25;
-output = img+noise;
-imshow(output);
--- Udacity/Computer_vision/2A-L1 images as function/scale.m
+++
@@ -1,4 +0,0 @@
-% Write down a function to scaling the image
-function result = scale(img, value)
-  result = value .* img;
-endfunction
